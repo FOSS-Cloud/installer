841912211af7ce9d0aa52809db2416c6802e00a8
Christian Affolter <christian.affolter@stepping-stone.ch>
2013-02-28 10:42:07 +0100

    Removing single server installation support for now

--- lib/installer.lib.sh
+++ lib/installer.lib.sh
@@ -127,14 +127,11 @@ function checkAvailableMemory ()
 function nodeTypeSelection ()
 {
     header "Node Type Selection"
-    info "The Installer supports four different types of servers:"
+    info "The Installer supports three different types of servers:"
     info ""
     info "-  The Demo-System which lets you quickly install and test the"
     info "   ${osbdProjectName} on a single machine without any further network"
     info "   requirements."
-    info ""
-    info "-  The Single-Server which runs the whole ${osbdProjectName} on"
-    info "   a single physical server, without any high availability."
     info ""
     info "-  The VM-Node which hosts the virtual machines in a multi node setup"
     info "   (requires at least four physical servers)."
@@ -144,13 +141,11 @@ function nodeTypeSelection ()
     info ""
     info "Please enter the number of the server type you would like to install"
     info "${osbdNodeTypeDemoSystem}) Demo-System"
-    info "${osbdNodeTypeSingleServer}) Single-Server"
     info "${osbdNodeTypeVmNode}) VM-Node (multi node setup)"
     info "${osbdNodeTypeStorageNode}) Storage-Node (multi node setup)"
      
     readVarAndValidateInList "osbdNodeType" "Node type" \
                              "${osbdNodeTypeDemoSystem} 
-                              ${osbdNodeTypeSingleServer}
                               ${osbdNodeTypeStorageNode}
                               ${osbdNodeTypeVmNode}"
 }
